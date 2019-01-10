22a2f44880459cda055c25bdb0700dac67f95610
Daniel Schultz <d.schultz@phytec.de>
2019-01-10 23:26:56 +0100

    Simple demo
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,9 +5,36 @@
  */
 
 #include <zephyr.h>
+#include <device.h>
+#include <gpio.h>
 #include <misc/printk.h>
+#include <misc/__assert.h>
+#include <string.h>
 
-void main(void)
+/* size of stack area used by each thread */
+#define STACKSIZE 1024
+
+/* scheduling priority used by each thread */
+#define PRIORITY 7
+
+#define LED_OK		0
+#define LED_WARN	1
+#define LED_ERROR	2
+#define LED_HEART	3
+
+void heartbeat(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	struct device *led_dev = device_get_binding("gpio1");
+	int val = 0;
+
+	gpio_pin_configure(led_dev, LED_HEART, GPIO_DIR_OUT);
+	while (1) {
+		gpio_pin_write(led_dev, LED_HEART, val);
+		val = !val;
+		k_sleep(1000);
+	}
+
 }
+
+K_THREAD_DEFINE(heartbeat_id, STACKSIZE, heartbeat, NULL, NULL, NULL,
+		PRIORITY, 0, K_NO_WAIT);
